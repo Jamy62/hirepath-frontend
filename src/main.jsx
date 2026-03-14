15638356b91a1254eb5f45aabed370b4f96a4c84
Jamy <jamy123ll@gmail.com>
2026-03-14 20:29:10 +0630

    changed root path in mainjs

--- src/main.jsx
+++ src/main.jsx
@@ -8,7 +8,7 @@ import './i18n';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/hirepath">
+    <BrowserRouter basename="/">
       <AuthProvider>
         <App />
       </AuthProvider>
